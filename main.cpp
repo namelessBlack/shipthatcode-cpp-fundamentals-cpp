72d584d509907c81f1f1844a929e906361f52bb8
user <shushagei@gmail.com>
2026-09-20 12:38:17 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <unordered_set>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
 
 int main() {
-    std::unordered_set<std::string> words;
-    std::string seen;
-    while (std::cin>>seen){
-        words.insert(seen);
+    std::vector<int> nums;
+    int x;
+
+    while (std::cin>>x) {
+        nums.push_back(x);
     }
-    std::cout << words.size() << "\n";
     
+    std::vector<int> sorted_nums;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] % 2 == 0) {
+            sorted_nums.push_back(nums[i]*nums[i]);
+        }
+    }
+
+    int sum = std::accumulate(sorted_nums.begin(), sorted_nums.end(), 0);
+    std::cout << sum;
+    return 0;
 }
